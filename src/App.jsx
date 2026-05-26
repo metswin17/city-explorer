a8b45c0d4b07eef579a50f8963e9c46c9f8a32c5
Luis Lopez <metfanforever@gmail.com>
2026-05-26 14:58:38 -0700

    step2 lab mod files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Explorer from './Explorer';
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Explorer />
-      <Footer />
-    </div>
-  );
+
+const [searchQuery, setSearchQuery] = useState('');
+const [location, setLocation] = useState({});
+
+async function getLocation() {
+  const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;  
+  const response = await axios.get(API);
+  
 }
 
+return (
+  <> 
+
+    <input
+    onChange={(e) => setSearchQuery(e.target.value)}
+    value={searchQuery}
+    placeholder='Enter a city name' />
+    <button onClick={getLocation}>Explore!</button> 
+    setLocation(response.data[0]);
+  {location.place_id && (
+
+     <div>
+    
+       <h2>{location.display_name}</h2>
+    
+     
+    
+       <p>Latitude: {location.lat}</p>
+    
+     
+    
+       <p>Longitude: {location.lon}</p>
+    
+     </div>
+  ) }
+
+  <div> className='App'
+    <Header />
+    <Explorer />
+    <Footer />
+   
+    </div> 
+  </>
+) } 
 export default App;
